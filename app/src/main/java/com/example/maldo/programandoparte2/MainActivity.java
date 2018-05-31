2b476e512ad5a58c8e9c6efcedd397add27a5989
maldonado.net <maldonado.net@hotmail.com>
2018-05-30 23:11:27 -0500

    Proyecto
    demo con bases de programacion
    if else y switch

---
+++ app/src/main/java/com/example/maldo/programandoparte2/MainActivity.java
@@ -0,0 +1,23 @@
+package com.example.maldo.programandoparte2;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.util.Log;
+import android.view.View;
+import android.widget.EditText;
+import android.widget.Toast;
+
+public class MainActivity extends AppCompatActivity {
+
+
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+
+    }
+
+
+}
